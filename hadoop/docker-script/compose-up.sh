1deecd2359308e54bc0b424519222320fe36ce1a
WojtekBoman <47774969+WojtekBoman@users.noreply.github.com>
2022-05-07 18:35:54 +0200

    Add ftpslave

--- hadoop/docker-script/compose-up.sh
+++ hadoop/docker-script/compose-up.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 hadoop_version=$1
-jdk_version=$2
-slaves=$3
-hdfs_path=$4
-log_path=$5
+slaves=$2
+hdfs_path=$3
+hadoop_log_path=$4
+hbase_log_path=$5
+hive_log_path=$6
+sqoop_log_path=$7
+maria_root_password=$8
+maria_data_path=$9
 
-if [ -z $log_path ]
+if [ -z $maria_data_path ]
 then
-  echo "Some parameter value is empty. Usage: compose-up.sh <hadoop_version> <jdk_version> <(The # of)slaves [integer]> <hdfs_path> <log_path>"
+  echo "Some parameter value is empty. Usage: compose-up.sh <hadoop_version> <(The # of) slaves [integer]> <hdfs_path> <hadoop_log_path> <hbase_log_path> <hive_log_path> <sqoop_log_path> <mariaDB_root_password> <mariaDB_data_path>"
   exit 1
 fi
 
@@ -41,26 +45,35 @@ master
 $workers
 EOF
 
+cat << EOF > regionservers
+$workers
+EOF
+
 for slave in $(seq 1 $slaves)
 do
-  ip_addr+='      - "'slave$slave':10.0.2.'$(($slave + 2))'"
+  ip_addr+='      - "'slave$slave':10.0.2.'$(($slave + 3))'"
 '
 done
 
 for slave in $(seq 1 $slaves)
 do
   slave_service+='  'slave$slave':
-    image: hjben/hadoop:'$hadoop_version'-jdk'$jdk_version'
+    image: hjben/hbase:1.6.0-hadoop'$hadoop_version'
     hostname: 'slave$slave'
     container_name: 'slave$slave'
     privileged: true
+    ports:
+      - '$((16029 + $slave))':16030
     volumes:
       - /sys/fs/cgroup:/sys/fs/cgroup
+      - '$hbase_log_path'/slave'$slave':/usr/local/hbase/logs
     networks:
       hadoop-cluster:
-        ipv4_address: 10.0.2.'$(($slave+2))'
+        ipv4_address: 10.0.2.'$(($slave + 3))'
     extra_hosts:
-      - "master:10.0.2.2"
+      - "mariadb:10.0.2.2"
+      - "master:10.0.2.3"
+      - "ftpslave:10.0.2.7"
 '$ip_addr
   if [[ ! $slave -eq $slaves ]]
   then
@@ -71,8 +84,52 @@ done
 
 cat << EOF > docker-compose.yml
 services:
+  ftp:
+    image: fauria/vsftpd
+    hostname: ftpslave
+    container_name: ftpslave
+    privileged: true
+    ports:
+      - "20:20"
+      - "21:21"
+      - "21100-21110:21100-21110"
+    environment:
+      FTP_USER: ftp
+      FTP_PASS: ftp
+      PASV_ADDRESS: 127.0.0.1
+      PASV_MIN_PORT: 21100
+      PASV_MAX_PORT: 21110
+    volumes:
+      - ./files:/home/vsftpd
+    networks:
+      hadoop-cluster:
+       ipv4_address: 10.0.2.7
+    extra_hosts:
+      - "mariadb:10.0.2.2"
+      - "master:10.0.2.3"
+      - "ftpslave:10.0.2.7"
+  mariadb:
+    image: hjben/mariadb:10.5
+    hostname: mariadb
+    container_name: mariadb
+    privileged: true
+    ports:
+      - 3306:3306
+    volumes:
+      - /sys/fs/cgroup:/sys/fs/cgroup
+      - $maria_data_path:/var/lib/mysql 
+    environment:
+      MARIADB_ROOT_PASSWORD: $maria_root_password
+    networks:
+      hadoop-cluster:
+        ipv4_address: 10.0.2.2
+    extra_hosts:
+      - "mariadb:10.0.2.2"
+      - "master:10.0.2.3"
+      - "ftpslave:10.0.2.7"
+$ip_addr
   master:
-    image: hjben/hadoop:$hadoop_version-jdk'$jdk_version'
+    image: hjben/hadoop-eco:$hadoop_version
     hostname: master
     container_name: master
     privileged: true
@@ -80,15 +137,23 @@ services:
       - 8088:8088
       - 9870:9870
       - 8042:8042
+      - 10000:10000
+      - 10002:10002
+      - 16010:16010
     volumes:
       - /sys/fs/cgroup:/sys/fs/cgroup
       - $hdfs_path:/data/hadoop
-      - $log_path:/usr/local/hadoop/logs
+      - $hadoop_log_path:/usr/local/hadoop/logs
+      - $hbase_log_path/master:/usr/local/hbase/logs
+      - $hive_log_path:/usr/local/hive/logs
+      - $sqoop_log_path:/usr/local/sqoop/logs
     networks:
       hadoop-cluster:
-        ipv4_address: 10.0.2.2
+        ipv4_address: 10.0.2.3
     extra_hosts:
-      - "master:10.0.2.2"
+      - "mariadb:10.0.2.2"
+      - "master:10.0.2.3"
+      - "ftpslave:10.0.2.7"
 $ip_addr
 $slave_service
 networks:
@@ -101,20 +166,32 @@ EOF
 echo "Done."
 
 echo "Docker-compose container run."
-echo "Remove old containers."
-docker-compose down --remove-orphans
-sleep 1
+# echo "Remove old containers."
+# docker-compose down --remove-orphans
+# sleep 1
 
 echo "Create new containers."
 docker-compose up -d
 sleep 1
 
 docker cp ./workers master:/usr/local/hadoop/etc/hadoop/workers
+docker cp ./regionservers master:/usr/local/hbase/conf/regionservers
 for slave in $(seq 1 $slaves)
 do
   docker cp ./workers slave$slave:/usr/local/hadoop/etc/hadoop/workers
+  docker cp ./regionservers slave$slave:/usr/local/hbase/conf/regionservers
 done
-echo "Done."
+
+cat << EOF > init-hive.sql
+create database hive;
+create user hive@'%' identified by 'hive';
+grant all privileges on hive.* to hive@'%';
+EOF
+
+docker cp init-hive.sql mariadb:/sh/
+docker exec -it mariadb bash -c "chmod 755 /sh/init-hive.sql"
 
 rm -f workers
-./hadoop-start.sh start
+rm -f regionservers
+rm -f init-hive.sql
+echo "Done."

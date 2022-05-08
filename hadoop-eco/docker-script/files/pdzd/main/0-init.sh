33335bee728aab9623a8469d3e05d115ffbca669
Piotr Jewłoszewicz <165008@student.pwr.edu.pl>
2022-05-08 21:56:44 +0200

    init script

--- hadoop-eco/docker-script/files/pdzd/main/0-init.sh
+++ hadoop-eco/docker-script/files/pdzd/main/0-init.sh
@@ -1,14 +1,47 @@
 #!/usr/bin/env bash
 
-# check if folders exist
-checkFolderExists() {
-  if [[ $(hdfs dfs -ls / | grep cars | wc -l) == 1 || $(hdfs dfs -ls / | grep geo | wc -l) == 1 ]]
-  then echo "exists"
+RF=$1
+CARS_DIR=$2
+GEO_DIR=$3
+
+checkDirectoryExists() {
+  echo "Looking for existing directories."
+  if [[ $(hdfs dfs -ls / | grep cars | wc -l) == 1 || $(hdfs dfs -ls / | grep geo | wc -l) == 1 ]]; then
+    cleanDirectories
   fi
 }
-checkFolderExists
-# if exists ask to continue
 
-# if continue removasdase all existing folders
+createCarsWithReplication() {
+  echo "Creating cars directory with ${RF} replicas."
+  hdfs dfs -mkdir -p /${CARS_DIR}
+  hdfs dfs -setrep -w ${RF} /${CARS_DIR}
+}
 
-# install directories and set replication factor
+createGeoWithReplication() {
+  echo "Creating geo directory with ${RF} replicas."
+  hdfs dfs -mkdir -p /${GEO_DIR}
+  hdfs dfs -setrep -w ${RF} /${GEO_DIR}
+}
+
+cleanDirectories() {
+  read -p "Would you like to re-create required directories? (y/N) " -n 1 -r
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    if [[ $(hdfs dfs -ls / | grep ${CARS_DIR} | wc -l) == 1 ]]; then
+      echo "Removing cars directory."
+      hdfs dfs -rm -r /${CARS_DIR}
+    fi
+
+    if [[ $(hdfs dfs -ls / | grep ${GEO_DIR} | wc -l) == 1 ]]; then
+      echo "Removing geo directory."
+      hdfs dfs -rm -r /${GEO_DIR}
+    fi
+    echo "Removing existing directories."
+  else
+    echo "Exiting."
+    exit 0
+  fi
+}
+
+checkDirectoryExists
+createCarsWithReplication
+createGeoWithReplication

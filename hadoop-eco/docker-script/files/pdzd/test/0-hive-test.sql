40751076f9997f6e42f9193e7e63faf76531be2c
Piotr Jewłoszewicz <165008@student.pwr.edu.pl>
2022-05-14 15:58:58 +0200

    hive test

---
+++ hadoop-eco/docker-script/files/pdzd/test/0-hive-test.sql
@@ -0,0 +1,6 @@
+create database tbTest;
+use tbTest;
+create table customer (id bigint, name string, address string);
+describe customer;
+insert into customer values (11, "test1", "test1"), (22, "test2", "test2");
+select * from customer;

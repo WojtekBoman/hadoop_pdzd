a32227da56dd432cf82ba0bb0c8301a1b90b7fca
root <root@LAPTOP-12RV62VN.localdomain>
2022-05-28 13:51:47 +0200

    Add 0-spark-test

--- hadoop-eco/docker-script/files/pdzd/test/0-spark-test.scala
+++ hadoop-eco/docker-script/files/pdzd/test/0-spark-test.scala
@@ -1,3 +1,11 @@
-var input = spark.read.textFile("hdfs://master:9000/cars/test_cars.csv")
-// Count the number of non blank lines
-input.filter(line => line.length()>0).count()
+import org.apache.spark.sql.functions.col
+
+val df = spark.read.option("header",true).csv("hdfs://master:9000/cars/test_cars.csv")
+
+df.coalesce(1).write.mode("overwrite").option("header", "true").csv("hdfs://master:9000/cars/test_cars_3.csv")
+
+df.select(col("firstSeen"),col("lastSeen")).show()
+
+val df2 = df.withColumn("dateDiff",datediff(col("lastSeen"), col("firstSeen"))).groupBy("brandName", "modelName", "vf_ModelYear").avg("dateDiff")
+
+df2.coalesce(1).write.mode("overwrite").option("header", "true").csv("hdfs://master:9000/cars/test_cars_2.csv")

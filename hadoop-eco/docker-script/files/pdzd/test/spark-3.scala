9a8424b6a6278f7d68bec991e1896e9ec9319997
root <root@LAPTOP-12RV62VN.localdomain>
2022-05-29 12:43:21 +0200

    Add fixes

--- hadoop-eco/docker-script/files/pdzd/test/spark-3.scala
+++ hadoop-eco/docker-script/files/pdzd/test/spark-3.scala
@@ -12,17 +12,21 @@ def operation_3a(df: DataFrame): DataFrame = {
 
 def operation_3b(df: DataFrame): DataFrame = {
         val df_with_diff = df.withColumn("dateDiff",datediff(col("lastSeen"), col("firstSeen"))).withColumn("diff", col("averageModelYearSaleTime") - col("dateDiff"))
-        df_with_diff.withColumn("modelYearDemandClass",when($"diff" > 0, when($"diff" > 50, "very_high").otherwise("high")).otherwise(when($"diff" < -50, "very_low").otherwise("low"))).select(col("firstSeen"),col("lastS$}
+        df_with_diff.withColumn("modelYearDemandClass",when($"diff" === 0, "medium")
+        .otherwise(when($"diff" > 0, when($"diff" > 50, "very_high").otherwise("high"))
+        .otherwise(when($"diff" < -50, "very_low").otherwise("low"))))
+        .select(col("firstSeen"),col("lastSeen"),col("modelName"),col("vf_modelYear"),col("modelYearDemandClass"))
+}
 
 val df = spark.read.option("header",true).csv("hdfs://master:9000/cars/test_cars.csv")
 
 val df_1 = operation_3a(df)
 val df_2 = operation_3b(df_1)
 
-df_2.coalesce(1).write.mode('overwrite').option('header','true').csv("hdfs://master:9000/tmps/tmp_3.csv")
+df_2.coalesce(1).write.mode("overwrite").option("header","true").csv("hdfs://master:9000/tmps/tmp_3.csv")
 
 val fs = FileSystem.get(sc.hadoopConfiguration)
 val file = fs.globStatus(new Path("hdfs://master:9000/tmps/tmp_3.csv/part*"))(0).getPath().getName()
-fs.rename(new Path("hdfs://master:9000/geo/tmp3.csv/" + file), new Path("hdfs://master:9000/geo/tmp_3.csv"))
-fs.delete(new Path("hdfs://master:9000/geo/tmp3/"), true)
+fs.rename(new Path("hdfs://master:9000/tmps/tmp3.csv/" + file), new Path("hdfs://master:9000/tmps/tmp_3.csv"))
+fs.delete(new Path("hdfs://master:9000/tmps/tmp3/"), true)
 

f9cc901155769b4179581ec7e0263b8186577753
m1kha3l <mdmichal197@gmail.com>
2022-05-29 23:16:44 +0200

    Update tmp1 test

--- hadoop-eco/docker-script/files/pdzd/test/4-attribute1-test.scala
+++ hadoop-eco/docker-script/files/pdzd/test/4-attribute1-test.scala
@@ -1,4 +1,4 @@
-val cars = spark.read.option("header",true).csv("hdfs://master:9000/test_data/cars.csv")
+val cars = spark.read.option("header",true).csv("hdfs://master:9000/test_data/cars.csv").select("msrp","askPrice")
 val carsWithPriceDiff = cars.withColumn("priceDiffPerc",(col("msrp")-col("askPrice"))/col("msrp")*100).select("msrp","askPrice","priceDiffPerc")
 
 val expectedResult = spark.read.option("header",true).csv("hdfs://master:9000/test_data/expected_result_1.csv")

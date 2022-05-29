c528dccdfd88f6903d620373a9aa47f25b663bb6
m1kha3l <mdmichal197@gmail.com>
2022-05-29 13:25:00 +0200

    Add tmp4.csv to HDFS

--- hadoop-eco/docker-script/files/pdzd/test/0-tmp4-spark.scala
+++ hadoop-eco/docker-script/files/pdzd/test/0-tmp4-spark.scala
@@ -1,13 +1,20 @@
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.catalyst.plans._
 import org.apache.hadoop.fs._
+import org.apache.spark.sql.functions.upper
 
 val cars = spark.read.option("header",true).csv("hdfs://master:9000/cars/test_cars.csv")
 val geo = spark.read.option("header",true).csv("hdfs://master:9000/geo/geo.csv")
-val joined = cars.join(geo, cars("vf_PlantCountry")===geo("name"),"inner")
+.withColumnRenamed("name", "vf_PlantCountry")
+.withColumn("vf_PlantCountry",upper(col("vf_PlantCountry")))
+.withColumn("vf_PlantCountry", regexp_replace(col("vf_PlantCountry"), "UNITED STATES", "UNITED STATES (USA)"))
+
+val joined = cars.join(geo, Seq("vf_PlantCountry"),"left_outer")
 joined.write.format("csv").save("hdfs://master:9000/tmps/tmp4")
 
 val fs = FileSystem.get(sc.hadoopConfiguration)
 val file = fs.globStatus(new Path("hdfs://master:9000/tmps/tmp4/part*"))(0).getPath().getName()
 fs.rename(new Path("hdfs://master:9000/tmps/tmp4/" + file), new Path("hdfs://master:9000/tmps/tmp4.csv"))
 fs.delete(new Path("hdfs://master:9000/tmps/tmp4"), true)
+
+System.exit(0)
